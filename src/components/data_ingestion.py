afcca1a78ca14f82b27c7d878c84bdcd774da2e1
Sandeep <sndpgauti@gmail.com>
2024-12-21 23:15:57 +0530

    Data Ingestion Added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from dataclasses import dataclass
+
+from sklearn.model_selection import train_test_split
+
+from src.logger import logging
+from src.exception import CustomException
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path:str=os.path.join('Artifacts',"raw_data.csv")
+    train_data_path:str=os.path.join('Artifacts',"train_data.csv")
+    test_data_path:str=os.path.join('Artifacts',"test_data.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Started!")
+        
+        try:
+            data=pd.read_csv("Notebook_Repository\\Data\\gemstone.csv")
+            logging.info("Read the data from CSV file")
+            
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Raw Data Created!")
+            
+            logging.info("Splitting the data into train and test")
+            
+            train_data, test_data=train_test_split(data, test_size=0.3, random_state=23)
+            logging.info("Data Splitting is Done!")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            
+            test_data.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info("train data and test date created!")
+            logging.info("Data Ingestion Completed!!")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured during data ingestion")
+            
+            raise CustomException(e,sys)
+    
+
+
