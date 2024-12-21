9542e9783ac6ec818a4cfb9c6142e16c1b178d32
Sandeep <sndpgauti@gmail.com>
2024-12-21 12:20:36 +0530

    Logger Added

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  #[2023-11-25 00:08:50,212] 24 root - INFO - Logging has started
+    level=logging.INFO,
+)
