925be5b839752e277be7df775501ce8a4866d8ad
Sandeep <sndpgauti@gmail.com>
2024-12-22 17:41:18 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,9 @@ from src.exception import CustomException
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     raw_data_path:str=os.path.join('Artifacts',"raw_data.csv")
@@ -60,7 +63,12 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data, test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr=data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr, test_arr))
+    
+    
     
 
 
